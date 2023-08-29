ff48d5f782fb976163e6ed0d9a62c5d510ac5dd3
satyaprakash04 <bullasatyaprakash@gmail.com>
2023-08-29 15:12:18 +0530

    second

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "KING OF DEVIL SATYA" );
+        System.out.println( "WELCOME TO DIET " );
     }
 }
