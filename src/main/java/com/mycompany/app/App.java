4234ae0613a4b1bca9c5e7d787b36058fd2a2948
satyaprakash04 <bullasatyaprakash@gmail.com>
2023-08-30 09:37:06 +0530

    third

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "WELCOME TO DIET " );
+        System.out.println( "WELCOME TO WORKSHOP 2nd DAY" );
     }
 }
